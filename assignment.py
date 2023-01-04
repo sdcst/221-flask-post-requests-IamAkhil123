7afc2bd26bd98faf12aa1331b8299c92eef71ae2
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2023-01-04 10:41:24 -0800

    d

--- assignment.py
+++ assignment.py
@@ -1,3 +1,31 @@
 #!python3
-from flask import request
-payload = request.form
+import random as r 
+from flask import Flask, request
+from flask_cors import CORS
+import time, json 
+import random 
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=['POST'])
+def main():
+      x = random.randint(0)
+      file = 'quotes.txt'
+      t = open(file, 'r')
+      data = t.read()
+      mydata = data.split('\n')
+      a = mydata[x]
+      return a 
+
+
+@app.route("?/admin", methods=['POST'])
+def admin():
+    t = open('quotes.txt', 'r')
+    data = t.read()
+    mydata = data.split('\n')
+    return mydata
+app.run()   
+
+
+
