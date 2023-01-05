f64ba8a259554be4aaa83bc457b573f08d48cb21
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2023-01-05 10:45:28 -0800

    d

--- assignment.py
+++ assignment.py
@@ -1,28 +1,89 @@
 #!python3
-import random as r 
-from flask import Flask, request
+#!python
+from flask import Flask,request
 from flask_cors import CORS
-import time, json 
-import random 
-
+import json
+import sqlite3
+import random
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/", methods=["POST"])
+@app.route("/going",methods=["POST"])
+def postResponse():
+    nop=True
+    # a simple echo server that response with the payload received.
+    payload = request.form
+    print(request.form)
+    print(payload)
+    data = dict(payload)
+    print(data)
+    saf=data['get']
+    if saf!='bce52a22495d704233dd4e720e942dbf63d0561ed813d77fd38776357db7108e':
+        return 'incorect'
+    file = 'quoteholder.db'
+    connection = sqlite3.connect(file)
+    print(connection)
+
+    cursor = connection.cursor()
+    query = "select quote from customers"
+    cursor.execute(query)
+    result = cursor.fetchall()
+    wild=[]
+    for g in result:
+        g=g[0]
+        wild.append(g.split(' '))
+    new=data['quote']
+    new=new.split(',')
+    nu=0
+    for i in result:
+        i=i[0]            
+        if i in new:
+            new.remove(i)
+            nop==False
+            che=False
+        elif nop==True:
+            che=True
+    if che==True:
+        for l in new:
+            wild=new[nu].split(' ')
+            x=len(wild)
+            x=round(x/2)+random.randint(-1,1)
+            if x>=len(wild):
+                x=round(x/2)
+            che=''.join(['%', f'{wild[1]}', '%'])
+            w=wild[-1].split('-')
+            che2=''.join(['%', f'{w[0]}', '%'])
+            che3=''.join(['%', f'{wild[x]}', '%'])
+            kil=0
+            if che2==che3 or che==che2:
+                queries=[che,che3]
+            else:
+                queries=[che,che2,che3]
+            for j in queries:
+                q = f"select * from customers where quote like '{j}'"
+                r = cursor.execute(q)
+                data = list(r)
+                if data!=[]:
+                    kil=kil+1
+                print(j, data)
+                if kil==2:
+                    new.remove(l)
+    data2 = [
+        new
+        ]
+    if data2==[[]]:
+        return'quote alredy in the system'
+    for i in data2:
+        i=i[0]
+        query = f"insert into customers (quote) values ('{i}')" 
+        print(query)
+        cursor.execute(query)
+    connection.commit()
+    
+    
+    return json.dumps(data2)
+@app.route("/",methods=['POST','GET'])
 def main():
-    x = random.randint(0)
-    file = 'quotes.txt'
-    t = open(file, 'r')
-    data = t.read()
-    mydata = data.split('\n')
-    a = mydata[x]
-    return a 
+    return "hi"
 
-@app.route("?/admin", methods=["POST"])
-def admin():
-    t = open('quotes.txt', 'r')
-    data = t.read()
-    mydata = data.split('\n')
-    return mydata
-app.run()   
-
+app.run("0.0.0.0")
