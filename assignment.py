10a2b18cf7dd1c295c5787116784980489730cbb
IamAkhil123 <90715959+IamAkhil123@users.noreply.github.com>
2023-01-30 11:58:39 -0800

    update
    update

--- assignment.py
+++ assignment.py
@@ -1,4 +1,6 @@
 #!python
+#221 flask-post-requests
+
 from flask import Flask, request, render_template
 from flask_cors import CORS
 import random
@@ -8,7 +10,7 @@ web = Flask(__name__)
 CORS(web)
 
 def textfile():
-    quotes = ["My unmatched perspicacity coupled with sheer indefatigability makes me a feared opponent in any realm of human endeavor.", "The truth is crazy in a world full of lies", ]
+    quotes = ["Spread love everywhere you go. Let no one ever come to you without leaving happier.","When you reach the end of your rope, tie a knot in it and hang on.","Always remember that you are absolutely unique. Just like everyone else."]
     file = open("quotes.txt", "w")
     for i in quotes:
         i +="\n"
@@ -16,21 +18,15 @@ def textfile():
 
 @web.route("/",methods=['POST','GET'])
 def main():
-    #sample url: http://127.0.0.1:5000/
-    #output: a text string literal, includes information for help
-    #response is not json encoded
-    file = open("entry.txt", "r")
+    file = open("quotes.txt", "r")
     quotes = file.readlines()
     quote = random.choice(quotes)
-    return f"As once said, '{quote}'"
+    return f'{quote}'
     
 @web.route("/admin",methods=['POST','GET'])
 def admin():
-    #sample url: http://127.0.0.1:5000/
-    #output: a text string literal, includes information for help
-    #response is not json encoded\
     global file
-    file = open('entry.txt', 'r')
+    file = open('quotes.txt', 'r')
     def working():
         Lines = file.readlines()
         count = 0
@@ -46,10 +42,7 @@ def admin():
 
 @web.route("/new",methods=['POST','GET'])
 def new():
-    #sample url: http://127.0.0.1:5000/
-    #output: a text string literal, includes information for help
-    #response is not json encoded\
-    file = open("entry.txt", "r")
+    file = open("quotes.txt", "r")
     quotes = file.readlines()
     count = -1
     for line in quotes:
@@ -60,19 +53,16 @@ def new():
 
 @web.route("/newquotes",methods=['POST','GET'])
 def newquote():
-    #sample url: http://127.0.0.1:5000/
-    #output: a text string literal, includes information for help
-    #response is not json encoded\
-    file = open("entry.txt", "r")
+    file = open("quotes.txt", "r")
     quotes = file.readlines()
     new_data = request.form
     new_data = dict(new_data)
     if new_data['val1'] not in quotes:
-        with open('entry.txt', 'a') as i:
+        with open('quotes.txt', 'a') as i:
             i.write(f"{new_data['val1']}\n")
             return "addition a success"
     else:
         print("Already entered")
 
 textfile()
-web.run()   
+web.run()
